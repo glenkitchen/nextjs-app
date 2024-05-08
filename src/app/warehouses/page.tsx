97299fc7cbda19bac121a4159ae32430e59e022e
Glen Kitchen <glen.kitchen@gmail.com>
2024-05-08 14:35:22 +0200

    web-api class

--- src/app/warehouses/page.tsx
+++ src/app/warehouses/page.tsx
@@ -2,18 +2,5 @@ import { webApi } from "@/utils/web-api";
 import AgGrid from "./warehouses-aggrid";
 
 export default async function Page() {
-  const rowData = [
-    {
-      code: "W1",
-      name: "Warehouse 1",
-    },
-    {
-      code: "W2",
-      name: "Warehouse 2",
-    },
-  ];
-
-  const rowData2 = await webApi.getRowData("distributioncenter");
-
   return <AgGrid rowData={await webApi.getRowData("distributioncenter")} />;
 }
--- src/utils/web-api.ts
+++ src/utils/web-api.ts
@@ -1,7 +1,11 @@
-export const webApi = {
-  getRowData: async (url: string) => {
-    const response = await fetch(`https://localhost:5001/api/${url}`);
+class WebApi {
+  private baseUrl = "https://localhost:5001/api/";
+
+  async getRowData(url: string) {
+    const response = await fetch(`${this.baseUrl}${url}`);
     const json = await response.json();
     return json?.data || [];
-  },
-};
+  }
+}
+
+export const webApi = new WebApi();
