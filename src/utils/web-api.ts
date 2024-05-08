f0b906e4d6349dd0e3c30a45814080b6451a9b3a
Glen Kitchen <glen.kitchen@gmail.com>
2024-05-08 15:14:03 +0200

    web-api

--- src/utils/web-api.ts
+++ src/utils/web-api.ts
@@ -1,11 +1,18 @@
+import { unstable_cache } from "next/cache";
+
 class WebApi {
+  //TODO env variable
   private baseUrl = "https://localhost:5001/api/";
 
-  async getRowData(url: string) {
+  //TODO header bearer token
+  private rowData = async (url: string) => {
     const response = await fetch(`${this.baseUrl}${url}`);
     const json = await response.json();
     return json?.data || [];
-  }
+  };
+
+  public getRowData = unstable_cache(this.rowData);
+  //public getRowData = this.rowData;
 }
 
 export const webApi = new WebApi();
