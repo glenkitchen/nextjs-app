ada050e4427fade78df5bf49589fb570ddc5774a
Glen Kitchen <glen.kitchen@gmail.com>
2024-05-13 09:28:10 +0200

    authorized callback

--- src/app/regions/page.tsx
+++ src/app/regions/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
+import getSession from "@/utils/get-session";
+
+export default async function Page() {
+  const session = await getSession();
+
+  if (!session) {
+    return <p>Not authenticated</p>;
+  }
+
   return <div>Regions</div>;
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,16 @@
 import NextAuth from "next-auth";
 import Okta from "next-auth/providers/okta";
+import { NextResponse } from "next/server";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Okta],
+  callbacks: {
+    async authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      if (pathname !== "/" && !auth) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+      return true;
+    },
+  },
 });
