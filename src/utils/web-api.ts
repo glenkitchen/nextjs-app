254fec4fb6878395344c7007e27a9854b62e3221
Glen Kitchen <glen.kitchen@gmail.com>
2024-05-13 14:58:58 +0200

    session?.access_token

--- src/utils/web-api.ts
+++ src/utils/web-api.ts
@@ -1,18 +1,35 @@
-// TODO
-// next-auth
-// error
+import getSession from "./get-session";
 
 class WebApi {
   private baseUrl = process.env.NEXT_PUBLIC_WEB_API_URL;
 
   public getRowData = async (url: string) => {
-    const response = await fetch(`${this.baseUrl}${url}`);
+    const session = await getSession();
+
+    const headers: HeadersInit = {
+      Authorization: `Bearer ${session?.access_token}`,
+    };
+
+    const response = await fetch(`${this.baseUrl}${url}`, {
+      headers,
+      cache: "no-cache",
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP getRowData. Status: ${response.status}`);
+    }
+
     const json = await response.json();
     return json?.data || [];
   };
 
   public getData = async (url: string) => {
     const response = await fetch(`${this.baseUrl}${url}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP getData. Status: ${response.status}`);
+    }
+
     const json = await response.json();
     return json || {};
   };
