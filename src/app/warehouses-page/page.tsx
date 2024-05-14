be28e6a05a6f1e45083ba5adea580cd2a5f9f654
Glen Kitchen <glen.kitchen@gmail.com>
2024-05-14 14:56:13 +0200

    remove warehouses-page

--- src/app/warehouses-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import { webApi } from "@/utils/web-api";
-import AgGrid from "./warehouses-aggrid";
-
-export default async function Page() {
-  return <AgGrid rowData={await webApi.getRowData("distributioncenter")} />;
-}
