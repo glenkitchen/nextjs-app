e80960039789d0683e167958add148ff62965498
Glen Kitchen <glen.kitchen@gmail.com>
2024-05-21 17:56:38 +0200

    drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import exp from "constants";
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
